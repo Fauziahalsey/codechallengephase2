13387d390322682b7ec479501d870ef996e55f47
Fauziahalsey <fauziaomala19@gmail.com>
2023-07-24 10:02:01 +0300

    addition of the url links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// App.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Transtab from './Component/Transtab';
 import Transform from './Component/Transform';
 import Search from './Component/Search';
@@ -7,18 +6,20 @@ import Summary from './Component/Summary';
 import AccountForm from './Component/AccountForm';
 
 const App = () => {
-  const [transactions, setTransactions] = useState([
-    // Sample data for demonstration
-    { id: 1, date: '2023-07-15', description: 'Food', amount: 2500 },
-    { id: 2, date: '2023-07-16', description: 'Transport', amount: -100 },
-    { id: 3, date: '2023-07-17', description: 'Housing', amount: -975 },
-    { id: 4, date: '2023-07-17', description: 'Entertainment', amount: -13.25 },
-    { id: 5, date: '2023-08-17', description: 'Gift', amount: 50 },
-
-    // Add more transactions here as needed
-  ]);
-
+  const [transactions, setTransactions] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    
+    fetch('http://localhost:3000/transactions')
+      .then(response => response.json())
+      .then(data => {
+        setTransactions(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   const addTransaction = (newTransaction) => {
     setTransactions([...transactions, newTransaction]);
@@ -35,14 +36,13 @@ const App = () => {
 
   return (
     <div className="container">
-    <h1>FlatIron Bank Transactions</h1>
-    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-    <Transtab transactions={filteredTransactions} deleteTransaction={deleteTransaction} />
-    <Transform addTransaction={addTransaction} />
-    <Summary transactions={filteredTransactions} /> 
-    <AccountForm addTransaction={addTransaction} /> 
-  </div>
-  
+      <h1>FlatIron Bank Transactions</h1>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <Transtab transactions={filteredTransactions} deleteTransaction={deleteTransaction} />
+      <Transform addTransaction={addTransaction} />
+      <Summary transactions={filteredTransactions} /> 
+      <AccountForm addTransaction={addTransaction} /> 
+    </div>
   );
 };
 
